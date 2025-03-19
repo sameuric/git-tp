70ff0a52c84a859067ae2d377b77a8622b279226
sameuric <78437091+sameuric@users.noreply.github.com>
2025-03-19 11:04:14 +0100

    added application

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 
+import logging
+
+
+def configLogging():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def main():
-    print("Hello, Git!")
+    configLogging()
+    # Envoi de messages avec différents niveaux de gravité
+    logging.debug('Ceci est un message de débogage')
+    logging.info('Ceci est un message informatif')
+    logging.warning('Ceci est un avertissement')
+    logging.error('Ceci est une erreur')
+    logging.critical('Ceci est un message critique')
 
 
 if __name__ == '__main__':
